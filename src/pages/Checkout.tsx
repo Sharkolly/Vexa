6a867606ed4f76b3c57185749a3760d138d3f359
Sharkolly <sharkollymofeoluwa@gmail.com>
2026-06-15 09:37:54 +0100

    feat: new change to the pages ui

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useAuthContextStore } from "../../store/useAuthContext";
 import { useSelector } from "react-redux";
 import SearchNav from "../../components/ui/SearchNav";
