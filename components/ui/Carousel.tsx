a4a8f48f634f3a644a8aec7701a9689d61d84436
Adesanya Mofeoluwa ( Folajimi ) <90891315+Sharkolly@users.noreply.github.com>
2026-08-04 12:25:42 +0100

    Add fallback image handling in Carousel component

--- components/ui/Carousel.tsx
+++ components/ui/Carousel.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import type { AllProductType } from "../../types/product.types";
 
 const Carousel = ({ product }: { product: AllProductType | null }) => {
+ const fallback = product?.images[0] || undefined
   const resolveImage = (
     img: string | File | null | undefined,
-    fallback: string | undefined
+    //fallback: string | undefined
     // fallback: string
   ) =>
      typeof img === "string"
@@ -84,7 +85,8 @@ const media = [...(product?.images ?? []), product?.video ?? null]
                 data-alt={product?.description}
                 src={resolveImage(
                   //   product?.images?.[carouselNumber] ?? undefined,
-                  media[carouselNumber], media[0] 
+                  media[carouselNumber], 
+                  //media[0] 
                   // 'https://lh3.googleusercontent.com/aida-public/AB6AXuADpcuApAMNwgTNOJuk0lE8Missb02pVKQbFi3oA_cyXAssxt5GcreNbWEFuDdo4ZVW3LaDWWC7jxoT60kK6VSslKL8LcCDS7YHvcqfjYwISqnsTqT18kOpV-eGpJUAh3E_dpheOaKTQ9pFzN_beS7ZboTw9R6UnBBACBqu5Fdhs6dfAm5WVDV5yVjCh_6J6k5_7MlSNhMR2J9mqHBdTQ9DkQQ8E7sDsyt9yKEydvb0tcllN8Z9V7Cv7FkEhYz16yRcbeyPENlTjjY'
                   // updatedMedia[carouselNumber] ?? undefined,
                    )}
