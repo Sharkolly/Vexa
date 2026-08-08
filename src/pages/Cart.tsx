655c8651067019d7805bd3a86202eb204c98f9e6
Adesanya Mofeoluwa ( Folajimi ) <90891315+Sharkolly@users.noreply.github.com>
2026-08-08 11:23:04 +0100

    Fix typo in class name for layout styling

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -212,7 +212,7 @@ const Cart = () => {
                                 {product.category} / {product.subCategory}
                               </p>
                             </Link>
-                            <div className="flex justify-between items-center">
+                            <div className="flex justify-betwee gap-6 items-center">
                               <p className=" font-semibold max-md:text-sm text-slate-600 ">
                                 ₦{product.price.toLocaleString()}
                               </p>
