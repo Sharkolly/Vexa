424300b6d95c53b4baa519d5ce8cba9b09a7ae47
Adesanya Mofeoluwa ( Folajimi ) <90891315+Sharkolly@users.noreply.github.com>
2026-08-12 18:45:12 +0100

    Fix button attribute from 'disable' to 'disabled'

--- src/pages/Delivery.tsx
+++ src/pages/Delivery.tsx
@@ -297,7 +297,7 @@ const destination = deliveryDetails.address
             </button>
           </Link>
           }
-            <button disable={calculating} className="w-full outline-none py-3 mb-4 mt-6 bg-green-800/90 text-white  rounded-xl shadow-lg hover:-translate-y-0.5 transition-all duration-300 active:scale-[0.98] cursor-pointer flex items-center justify-center  gap-2" onClick={calcDistance}>
+            <button disabled={calculating} className="w-full outline-none py-3 mb-4 mt-6 bg-green-800/90 text-white  rounded-xl shadow-lg hover:-translate-y-0.5 transition-all duration-300 active:scale-[0.98] cursor-pointer flex items-center justify-center  gap-2" onClick={calcDistance}>
               <span>
                 <IoBagCheckOutline className="text-white w-5 h-5" />
               </span>
