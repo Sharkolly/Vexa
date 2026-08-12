c16ff838097baf435822eaedf20114efb560f3ef
Adesanya Mofeoluwa ( Folajimi ) <90891315+Sharkolly@users.noreply.github.com>
2026-08-12 18:51:03 +0100

    Change button color from green to yellow

--- src/pages/Delivery.tsx
+++ src/pages/Delivery.tsx
@@ -299,8 +299,8 @@ const destination = deliveryDetails.address
             </button>
           </Link>
           }
-            <button disabled={calculating} className="w-full outline-none py-3 mb-4 mt-6 bg-green-800/90 text-white  rounded-xl shadow-lg hover:-translate-y-0.5 transition-all duration-300 active:scale-[0.98] cursor-pointer flex items-center justify-center  gap-2" onClick={calcDistance}>
-              <span>
+            <button disabled={calculating} className="w-full outline-none py-3 mb-4 mt-6 bg-yellow-800/90 text-white  rounded-xl shadow-lg hover:-translate-y-0.5 transition-all duration-300 active:scale-[0.98] cursor-pointer flex items-center justify-center  gap-2" onClick={calcDistance}>
+              <span className='hidden'>
                 <IoBagCheckOutline className="text-white w-5 h-5" />
               </span>
               <p> {calculating ? 'Calculating' : 'Calculate Delivery Fee'}</p>
