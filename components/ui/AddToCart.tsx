3199f245e41b6848046bcc5bc704c78b2f89e6b5
Adesanya Mofeoluwa ( Folajimi ) <90891315+Sharkolly@users.noreply.github.com>
2026-08-26 05:52:06 +0100

    Adjust visibility breakpoint for ShoppingCart icon

--- components/ui/AddToCart.tsx
+++ components/ui/AddToCart.tsx
@@ -116,7 +116,7 @@ const AddToCart = ({
       }
       onClick={handleAddToCart}
     >
-      <ShoppingCart className="max-[500px]:hidden w-4 h-4" />
+      <ShoppingCart className="max-[440px]:hidden w-4 h-4" />
       <span>Add to Cart</span>
     </button>
   );
